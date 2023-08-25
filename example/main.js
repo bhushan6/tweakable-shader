ea071d2f559743dcdf56c209086075ca8317dde9
bhushan6 <bhushanwagh6@gmail.com>
2023-08-25 20:14:22 +0530

    updates example

--- example/main.js
+++ example/main.js
@@ -62,3 +62,35 @@ const shaderMaterial = new TweakableShader("shader-material", scene, {
 });
 
 box.material = shaderMaterial;
+
+const box2 = BABYLON.MeshBuilder.CreateBox("box", { size: 2 });
+
+const shaderMaterial2 = new TweakableShader("shader-material2", scene, {
+  vertexShader: `precision highp float;
+                attribute vec3 position;
+                uniform mat4 worldViewProjection;
+                attribute vec2 uv;
+
+                varying vec2 vUv;
+
+                void main() {
+                    vec4 p = vec4(position, 1.);
+                    gl_Position = worldViewProjection * p;
+                    vUv = uv;
+                }`,
+  fragmentShader: `precision highp float;
+                uniform float brightness; // ts({ value: 1.0, min: 0, max:1.0, step: 0.01 })
+                uniform float uTime;
+
+                varying vec2 vUv;
+
+                void main() {
+                    gl_FragColor = vec4(vUv.x * (sin(uTime * 0.01) + 1.) - 1., vUv.y, (sin(uTime * 0.01) + 1.) - 1. , brightness);
+                }`,
+  needAlphaBlending: true,
+  timeUniform: { name: "uTime" },
+});
+
+box2.material = shaderMaterial2;
+box2.position.x = -2;
+box.position.x = 2;
